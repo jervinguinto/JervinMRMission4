db74d1b7411cfc9ee2868eb8b3506e70559405c6
jervinguinto <jervin.guinto052894@gmail.com>
2022-12-20 17:35:53 +1300

    added key on res

--- index.js
+++ index.js
@@ -4,40 +4,26 @@ const calculatedCarValue = require("./CarValue");
 const express = require("express");
 const server = express();
 server.use(express.json());
-const env = require("dotenv")
-env.config()
-const PORT = process.env.PORT || 4002
-
+const env = require("dotenv");
+env.config();
+const PORT = process.env.PORT || 4002;
 
 server.listen(PORT, () => {
   console.log("listening to port", PORT);
 });
 
-server.get("/", (req,res) => {
+server.get("/", (req, res) => {
   res.send("this is working");
 });
 
-// function carValue(req, res) {
-//   let carModel;
-//   let carYear;
-//   let carValue;
-//   ({ carModel, carYear } = req.body);
-
-//   carValue = calculatedCarValue(carModel, carYear);
-//   res.json(carValue);
-// }
-
-// server.post("/carValue",carValue);
-
-
-server.post("/carValue",(req, res) =>{
+function carValue(req, res) {
   let carModel;
   let carYear;
   let carValue;
   ({ carModel, carYear } = req.body);
 
-  res.json(calculatedCarValue(carModel, carYear));
-  
+  carValue = calculatedCarValue(carModel, carYear);
+  res.json({"carValue":carValue})
 }
 
-)
+server.post("/carValue", carValue);
